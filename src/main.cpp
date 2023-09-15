6e164aa024d4ac5280bc67c3b11f0ac8e3a73864
Taher Alkhateeb <taher@pythys.com>
2023-09-15 18:46:11 +0300

    got CLI11 imported and working properly, sweet

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <CLI/CLI.hpp>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    CLI::App app("CLI11 demo application");
+    std::string name;
+    app.add_option("--name", name, "Your name");
+
+    try {
+        app.parse(argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return app.exit(e);
+    }
+
+    if(!name.empty()) {
+        std::cout << "Hello, " << name << "!\n";
+    } else {
+        std::cout << "Hello, World!\n";
+    }
+
     return 0;
 }
+
