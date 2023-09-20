f82be84ded60fffe7af29f8db71e0b96cde4000d
Taher Alkhateeb <taher@pythys.com>
2023-09-20 19:14:35 +0300

    handle compiler

--- src/exec.cpp
+++ src/exec.cpp
@@ -1,9 +1,11 @@
 #include <string>
+#include <iostream>
 
 int run_exec(
     const std::string &custom_command,
     const std::string &config_file,
     const std::string &repo_type) {
 
+    std::cout << custom_command + config_file + repo_type;
     return 0;
 }
