384db7453ae3ec81d75195edfb2df5d208f52d9e
Taher Alkhateeb <taher@pythys.com>
2023-09-21 09:03:06 +0300

    got basic looping structure

--- src/sync.cpp
+++ src/sync.cpp
@@ -1,11 +1,16 @@
-#include "sync.hpp"
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <yaml-cpp/yaml.h>
+#include "config.hpp"
+#include "sync.hpp"
+#include "tree.hpp"
 
 int run_sync(const std::string& config_file) {
-    std::cout << "Running sync with config: "
-              << config_file
-              << std::endl;
+    std::vector<Tree> config = get_config(config_file);
+    // TODO replace with parallel transform
+    std::for_each(config.begin(), config.end(), [](const Tree& tree) {
+        std::cout << tree.root << std::endl;
+        std::cout << tree.repos.size() << std::endl;
+    });
     return 0;
 }
