79822c32a03cd425b990ea004133091d34676eca
Taher Alkhateeb <taher@pythys.com>
2023-10-21 22:10:28 +0300

    test different protocols

--- tests/config_tests.cpp
+++ tests/config_tests.cpp
@@ -5,7 +5,17 @@ std::vector<Tree> parse_config(const std::string& filename) {
     return get_config(std::string(TEST_RESOURCES_DIR) + "/" + filename);
 }
 
-TEST(ConfigTests, TreesRecognition) {
+TEST(ConfigTests, HttpsProtocolIdentified) {
     std::vector<Tree> trees = parse_config("git_protocols.yml");
-    EXPECT_EQ(trees.size(), 3);
+    EXPECT_EQ(trees[0].repos[0].remotes[0].type, RemoteType::HTTPS);
+}
+
+TEST(ConfigTests, GitProtocolIdentified) {
+    std::vector<Tree> trees = parse_config("git_protocols.yml");
+    EXPECT_EQ(trees[1].repos[0].remotes[0].type, RemoteType::GIT);
+}
+
+TEST(ConfigTests, SshProtocolIdentified) {
+    std::vector<Tree> trees = parse_config("git_protocols.yml");
+    EXPECT_EQ(trees[2].repos[0].remotes[0].type, RemoteType::SSH);
 }
