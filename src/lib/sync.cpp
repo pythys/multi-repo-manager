b63b93728b7b62e8e483f5cd792400c6869520a9
Taher Alkhateeb <taher@pythys.com>
2023-12-07 16:01:09 +0300

    implemented sync, to fix boost installation

--- src/lib/sync.cpp
+++ src/lib/sync.cpp
@@ -1,39 +1,44 @@
-#include <algorithm>
-#include <execution>
+#include <boost/asio.hpp>
 #include <filesystem>
 #include <iostream>
-#include <string>
 #include <vector>
 #include "config.hpp"
 #include "sync.hpp"
 #include "tree.hpp"
 
 namespace fs = std::filesystem;
+namespace asio = boost::asio;
+
+void sync_repository(
+    const std::string& root,
+    const Repo& repo,
+    asio::io_context& io_context,
+    asio::thread_pool& pool) {
+
+    const std::string repo_path = root + "/" + repo.name;
+    fs::path repo_dir("./" + repo_path);
+    auto sync_action = [&]() {
+        if (fs::exists(repo_dir) && fs::is_directory(repo_dir)) {
+            std::cout << "updating repo: " + repo_path << std::endl;
+        } else {
+            std::cout << "cloning repo:" + repo_path << std::endl;
+        }
+        for (auto& child : repo.children) {
+            sync_repository(repo_path, child, io_context, pool);
+        }
+    };
+    asio::post(pool, sync_action);
+}
 
 int run_sync(const std::string& config_file) {
     std::vector<Tree> config = get_dependencies(config_file);
-    /* TODO implement parallel algorithm as follows
-     * is path a directory and a repo ?
-     * - true: update the remotes
-     * - false: clone the repository
-     *
-     * The logic must wait for parent repos before cloning nested repos
-     */
-    std::for_each(std::execution::par,
-                  config.begin(),
-                  config.end(),
-                  [](const auto& tree) {
-        std::for_each(std::execution::par,
-                      tree.repos.begin(),
-                      tree.repos.end(),
-                      [&tree](const auto& repo) {
-            fs::path repo_dir(tree.root + "/" + repo.name);
-            if (fs::exists(repo_dir) && fs::is_directory(repo_dir)) {
-                std::cout << "path exists" << std::endl;
-            } else {
-                std::cout << "path does not exist" << std::endl;
-            }
-        });
-    });
+    asio::io_context io_context;
+    asio::thread_pool pool(10);
+    for (const auto& tree : config) {
+        for (const auto& repo : tree.repos) {
+            sync_repository(tree.root, repo, io_context, pool);
+        }
+    }
+    pool.join();
     return 0;
 }
