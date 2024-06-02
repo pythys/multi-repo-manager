f138608d29f8cba72933445fd1bd728cf7d55a55
Taher Alkhateeb <taher@pythys.com>
2024-06-02 14:30:35 +0300

    implement remotes logic

--- src/lib/sync.cpp
+++ src/lib/sync.cpp
@@ -12,6 +12,46 @@
 namespace fs = std::filesystem;
 namespace asio = boost::asio;
 
+enum class MatchType { TO_REMOVE, TO_ADD };
+std::vector<Remote> find_remotes(
+    const std::vector<Remote>& tree_remotes,
+    const std::vector<Remote>& git_remotes,
+    MatchType match_type) {
+
+    std::vector<Remote> result;
+
+    auto compare_by_name = [](const Remote& a, const Remote& b) {
+        return a.name == b.name;
+    };
+
+    if (match_type == MatchType::TO_REMOVE) {
+        for (const auto& git_remote : git_remotes) {
+            auto it = std::find_if(
+                tree_remotes.begin(),
+                tree_remotes.end(),
+                [&](const Remote& tree_remote) {
+                    return compare_by_name(tree_remote, git_remote);
+                });
+            if (it == tree_remotes.end()) {
+                result.push_back(git_remote);
+            }
+        }
+    } else if (match_type == MatchType::TO_ADD) {
+        for (const auto& tree_remote : tree_remotes) {
+            auto it = std::find_if(
+                git_remotes.begin(),
+                git_remotes.end(),
+                [&](const Remote& git_remote) {
+                    return compare_by_name(git_remote, tree_remote);
+                });
+            if (it == git_remotes.end()) {
+                result.push_back(tree_remote);
+            }
+        }
+    }
+    return result;
+}
+
 void sync_repository(
     const std::string& root,
     Repo* repo,
@@ -22,8 +62,20 @@ void sync_repository(
     auto update_action = [root, repo, &repo_manager]() {
         std::cout << "updating repo: " + root + "/" + repo->name << std::endl;
         auto remotes = repo_manager->get_remotes(root + "/" + repo->name);
-        // TODO(taher) add missing
-        // TODO(taher) remove extras
+        auto to_remove = find_remotes(
+            repo->remotes,
+            remotes,
+            MatchType::TO_REMOVE);
+        for (const auto& remote : to_remove) {
+            repo_manager->remove_remote(root + "/" + repo->name, remote);
+        }
+        auto to_add = find_remotes(
+            repo->remotes,
+            remotes,
+            MatchType::TO_ADD);
+        for (const auto& remote : to_add) {
+            repo_manager->add_remote(root + "/" + repo->name, remote);
+        }
     };
 
     std::promise<void> clone_completed;
