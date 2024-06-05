d7b02fc5d144d00eaf81215605d0dc47fd3c42fc
Taher Alkhateeb <taher@pythys.com>
2024-06-05 23:27:45 +0300

    traverse all dirs in find

--- src/lib/find.cpp
+++ src/lib/find.cpp
@@ -2,9 +2,17 @@
 #include <iostream>
 #include <string>
 #include "find.hpp"
+#include "tree.hpp"
 
 namespace fs = std::filesystem;
+
 int run_find(const std::string& path) {
+    fs::path root(path);
+    if (fs::exists(root) && fs::is_directory(root)) {
+        for (const auto& entry : fs::recursive_directory_iterator(root)) {
+            std::cout << entry.path() << std::endl;
+        }
+    }
     std::cout << "Running find with path: "
               << path
               << std::endl;
