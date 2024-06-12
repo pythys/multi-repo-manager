6c7a6c387a40ce8ff5e993769923ce156a3e4bdb
Taher Alkhateeb <taher@pythys.com>
2024-06-12 23:08:07 +0300

    add more tests

--- tests/config_tests.cpp
+++ tests/config_tests.cpp
@@ -9,6 +9,11 @@ std::vector<Tree> parse_dependencies(const std::string& filename) {
     return get_dependencies(std::string(TEST_RESOURCES_DIR) + "/" + filename);
 }
 
+TEST(ConfigTests, WithoutNesting) {
+    std::vector<Tree> trees = parse_config("nested_repos.yml");
+    EXPECT_EQ(trees[0].repos.size(), 4);
+}
+
 TEST(ConfigTests, NestingFirstLevel) {
     std::vector<Tree> trees = parse_dependencies("nested_repos.yml");
     EXPECT_EQ(trees[0].repos.size(), 1);
@@ -23,4 +28,9 @@ TEST(ConfigTests, NestingThirdLevel) {
     std::vector<Tree> trees = parse_dependencies("nested_repos.yml");
     EXPECT_EQ(trees[0].repos[0].children[0].children.size(), 1);
     EXPECT_EQ(trees[0].repos[0].children[1].children.size(), 0);
+}
+
+TEST(ConfigTests, MultipleTrees) {
+    std::vector<Tree> trees = parse_dependencies("multi_tree.yml");
+    EXPECT_EQ(trees.size(), 2);
 }
