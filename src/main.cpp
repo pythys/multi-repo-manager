67a652b3d1f658380d7bdeb7517f1ce1f46ab4ee
Taher Alkhateeb <taher@pythys.com>
2024-06-13 15:50:58 +0300

    move git init logic to a guarded header

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
-#include <memory>
 #include "cli.hpp"
-#include "git_manager.hpp"
+#include "git_guard.hpp"
 
 int main(int argc, char **argv) {
-    std::unique_ptr<GitManager> git_manager = std::make_unique<GitManager>();
-    git_manager->init();
+    GitGuard git_guard;
     int result = parse_cli(argc, argv);
-    git_manager->shutdown();
     return result;
 }
