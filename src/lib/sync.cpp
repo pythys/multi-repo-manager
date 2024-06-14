270dab34578e5456f4de2f474e5b7e75da4fe95e
Taher Alkhateeb <taher@pythys.com>
2024-06-14 22:57:02 +0300

    Got a basic skeleton of SyncScreen

--- src/lib/sync.cpp
+++ src/lib/sync.cpp
@@ -2,12 +2,14 @@
 #include <filesystem>
 #include <future>
 #include <iostream>
+#include <memory>
 #include <vector>
 #include <boost/asio.hpp>
 #include "config.hpp"
 #include "constants.hpp"
 #include "repo_factory.hpp"
 #include "sync.hpp"
+#include "sync_screen.hpp"
 #include "tracker.hpp"
 #include "tree.hpp"
 
@@ -144,6 +146,8 @@ void sync_repository(
 int run_sync(const std::string& config_file) {
     std::vector<Tree> config = get_dependencies(config_file);
     Tracker::get_instance().populate(config);
+    auto screen = std::make_unique<SyncScreen>();
+    Tracker::get_instance().add_observer(screen.get());
     asio::thread_pool pool(SYNC_POOL_SIZE);
     tbb::parallel_for_each(
         config.begin(),
---
+++ src/lib/sync_screen.hpp
@@ -0,0 +1,12 @@
+#ifndef SRC_LIB_SYNC_SCREEN_HPP_
+#define SRC_LIB_SYNC_SCREEN_HPP_
+
+#include <iostream>
+#include "tracker.hpp"
+
+class SyncScreen : public TreeObserver {
+ public:
+    void update() {/*TODO*/}
+};
+
+#endif  // SRC_LIB_SYNC_SCREEN_HPP_
