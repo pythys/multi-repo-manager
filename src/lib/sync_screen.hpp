28c812f9d7472755d1939d465bab7907698c70ba
Taher Alkhateeb <taher@pythys.com>
2024-11-24 10:57:19 +0400

    got first implementatino of FTXUI

--- src/lib/sync_screen.hpp
+++ src/lib/sync_screen.hpp
@@ -2,11 +2,31 @@
 #define SRC_LIB_SYNC_SCREEN_HPP_
 
 #include <iostream>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 #include "tracker.hpp"
 
 class SyncScreen : public TreeObserver {
  public:
-    void update() {/*TODO*/}
+    SyncScreen() = default;
+        void update() {
+            render();
+        }
+        void render() {
+            using ftxui::Renderer;
+            using ftxui::Component;
+            using ftxui::ScreenInteractive;
+            using ftxui::text;
+            using ftxui::center;
+            using ftxui::bold;
+
+            auto renderer = Renderer([] {
+                return text("Hello, SyncScreen!") | center | bold;
+            });
+
+            auto screen = ScreenInteractive::FitComponent();
+            //screen.Loop(renderer);
+        }
 };
 
 #endif  // SRC_LIB_SYNC_SCREEN_HPP_
