ce5afd1b5615d9c4b802013b84b22fe79be73a9a
Taher Alkhateeb <taher@pythys.com>
2024-12-25 23:29:34 +0400

    make updates parallel

--- src/lib/update.cpp
+++ src/lib/update.cpp
@@ -1,18 +1,26 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <boost/asio.hpp>
 #include "config.hpp"
+#include "constants.hpp"
 #include "repo_factory.hpp"
 #include "update.hpp"
 
+namespace asio = boost::asio;
+
 int run_update(const std::string& config_file) {
     std::vector<Tree> config = get_dependencies(config_file);
-    for (auto tree : config) {
-        for (auto repo : tree.repos) {
-            auto repo_manager = create_repo_manager(repo.type);
-            auto repo_path = tree.root + "/" + repo.name;
-            repo_manager->update(repo_path);
+    asio::thread_pool pool(SYNC_POOL_SIZE);
+    for (auto& tree : config) {
+        for (auto& repo : tree.repos) {
+            auto updater = [repo, tree] {
+                auto repo_manager = create_repo_manager(repo.type);
+                auto repo_path = tree.root + "/" + repo.name;
+                repo_manager->update(repo_path);
+            };
+            asio::post(pool, updater);
         }
     }
+    pool.join();
     return 0;
 }
